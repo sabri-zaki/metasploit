375fb81a0ec1dc08f8a85a35894a88357f80c3ec
sabri-zaki <37634605+sabri-zaki@users.noreply.github.com>
2019-05-01 18:54:12 +0100

    Update metasploit.sh

--- metasploit.sh
+++ metasploit.sh
@@ -14,7 +14,7 @@ if [ $name != "metasploit.sh" ]; then
 	exit 
 fi
 
-msfvar=4.17.23
+msfvar=4.15.0
 msfpath='/data/data/com.termux/files/home'
 if [ -d "$msfpath/metasploit-framework" ]; then
 	echo "metasploit is installed"
